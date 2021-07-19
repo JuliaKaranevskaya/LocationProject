c9f229cbd7549a6822bc0b0ca41d5986897752e3
Юлия <yuliya.karanevskaya@gmail.com>
2021-07-19 23:23:20 +0300

    core locationcpractice

--- LocationProject/ViewController.swift
+++ LocationProject/ViewController.swift
@@ -6,12 +6,54 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let map: MKMapView = {
+        let m = MKMapView()
+        return m
+    }()
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(map)
+        map.frame = view.bounds
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            locationManager.stopUpdatingLocation()
+            showLocation(location: location)
+        }
+    }
+    
+    func showLocation(location: CLLocation) {
+        
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        map.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        map.addAnnotation(pin)
+        
     }
 
 
